e6185f3d597d31af8c147706edeeb650777c8fa9
AdarshTiwari79 <jaunpuradarsh62@gmail.com>
2024-07-16 09:49:42 +0530

    initial commit with project creation using STS IDE

---
+++ backendvalidation/src/main/java/com/boot/backendvalidation/BackendvalidationApplication.java
@@ -0,0 +1,13 @@
+package com.boot.backendvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendvalidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendvalidationApplication.class, args);
+	}
+
+}
---
+++ backendvalidation/src/test/java/com/boot/backendvalidation/BackendvalidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.backendvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendvalidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
