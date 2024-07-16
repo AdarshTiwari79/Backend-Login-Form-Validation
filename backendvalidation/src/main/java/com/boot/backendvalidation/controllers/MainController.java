bb1a2f1d9d60e5ceb40f6db28b673f57ae204a89
AdarshTiwari79 <jaunpuradarsh62@gmail.com>
2024-07-16 12:45:14 +0530

    triggering validation with add parameter of @valid and storing BindResult

--- backendvalidation/src/main/java/com/boot/backendvalidation/controllers/MainController.java
+++ backendvalidation/src/main/java/com/boot/backendvalidation/controllers/MainController.java
@@ -2,11 +2,14 @@ package com.boot.backendvalidation.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.boot.backendvalidation.entities.LoginData;
+
+import jakarta.validation.Valid;
 
 
 
@@ -24,8 +27,12 @@ public class MainController {
 	
 	// to process form after submit
 	@PostMapping("/submit")
-	public String processForm(@ModelAttribute("loginData") LoginData loginData) {
+	public String processForm(@Valid @ModelAttribute("loginData") LoginData loginData, BindingResult result) {
 		
+		if(result.hasErrors()) {
+			System.out.println(result);
+			return "form";
+		}
 		System.out.println(loginData);
 		return "success";
 	}
